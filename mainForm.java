2d223264f03dd8fb5dbb7877310f3a8262a193af
Braden Dressendorfer <48044240+Brad3nD@users.noreply.github.com>
2019-02-27 00:29:54 -0600

    Add files via upload

---
+++ mainForm.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author slisk01
+ */
+public class mainForm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Login gui = new Login();
+            gui.show();        // TODO add your handling code he
+    }
+    
+}
